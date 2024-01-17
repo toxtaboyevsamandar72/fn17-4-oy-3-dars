6030c28c8de990d4378958eed29d5bad37da65b7
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-01-17 09:30:18 +0500

    index html hommework

---
+++ js/index.js
@@ -0,0 +1,136 @@
+// 1-MISOL
+
+
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let sum = 1;
+
+
+
+
+// if (num1 % 3 == 0) {
+//   sum *= num1;
+// }
+// if (num2 % 3 == 0) {
+//   sum *= num2;
+// }
+// if (num3 % 3 == 0) {
+//   sum *= num3;
+// }
+// document.write(`3ga bo'linadigan sonlar ko'paymasi_`,sum);
+
+
+
+
+
+// 2-MISOL
+
+
+
+
+// let number$ = +prompt("3-xonalik soni kiriting");
+// let hundreds$;
+// let ten$;
+// let units$;
+// let res$;
+// let sum$;
+// let sum$1;
+
+
+// sum$ = number$ % 100;
+// hundreds$= (number$ - sum$) / 100;
+// sum$1 = (number$%100)%10;
+// ten$ = ((number$%100) - sum$1) / 10;
+// units$ =number$%10;
+// res$ = hundreds$ + ten$ + units$;
+// document.write(res$);
+
+
+
+
+
+
+// 4-MISOL
+
+
+
+
+
+// let num_1 = +prompt("1-sonni kiriting");
+// let num_2 = +prompt("2-sonni kiriting");
+// let num_3 = +prompt("3-sonni kiriting");
+// let res_;
+
+
+
+// if (num_1 % 5 == 0) {
+//   if (num_1 % 7 == 0) {
+//     res_ = num_1;
+//     console.log(`1-son 5ga va 7ga bo'linadi-`, res_);
+//   }
+// }
+// if (num_2 % 5 == 0) {
+//   if (num_2 % 7 == 0) {
+//     res_ = num_2;
+//     console.log(`2-son 5ga va 7ga bo'linadi-`, res_);
+//   }
+// }
+// if (num_3 % 5 == 0) {
+//   if (num_3 % 7 == 0) {
+//     res_ = num_3;
+//     console.log(`3-son 5ga va 7ga bo'linadi-`, res_);
+//   }
+//
+
+
+
+
+// 5-MISOL 
+
+
+
+
+// const containsVowels = string => {
+
+//   var lowerCase = string.toLowerCase(r);
+
+
+//   var word = lowerCase.split("");
+
+
+//   var vowelsArray = ["a","o","i","u","y"];
+
+
+// const result = word.filter(letter => vowelsArray.includes(letter));
+
+// return result.includes("a","o","i","u","y");
+
+
+// };
+
+
+
+
+
+
+
+// 7-MISOL
+
+
+
+
+
+
+// let degree1 = +prompt("1-burchakni kiriting");
+// let degree2 = +prompt("2-burchakni kiriting");
+// let degree3 = +prompt("3-burchakni kiriting");
+// let res = "Uchburchak yasash mumkin emas ";
+
+
+// if (degree1 + degree2 + degree3 == 180) {
+//   res = "Uchburchak yasash mumkin";
+// }
+// document.write(res);
+
